b012ba6c73f89d76fecfc9a39260340633ff5b95
xInsomnian <98590267+xInsomnian@users.noreply.github.com>
2022-11-18 21:15:29 -0600

    Adding first file

---
+++ account.py
@@ -0,0 +1,25 @@
+class Account:
+    def __init__(self, name: str):
+        self.__account_name = name
+        self.__account_balance = 0
+
+    def deposit(self, amount: float) -> bool:
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount: float) -> bool:
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+    def get_balance(self) -> float:
+        return self.__account_balance
+
+    def get_name(self) -> str:
+        return self.__account_name
+    
