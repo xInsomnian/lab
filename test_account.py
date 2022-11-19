e0d4bb8e242340bbdd221834e9aaf5b91aa5de74
xInsomnian <98590267+xInsomnian@users.noreply.github.com>
2022-11-18 21:45:52 -0600

    updating testing file

--- test_account.py
+++ test_account.py
@@ -1,2 +1,14 @@
 import pytest
 from account import *
+class Test:
+
+    def setup_method(self):
+
+    def teardown_method(self):
+
+
+    def test_init(self):
+
+    def test_deposit():
+
+    def test_withdraw():
