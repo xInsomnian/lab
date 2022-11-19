bb300d3206ea9c0897e089d6a79eff013bddda46
xInsomnian <98590267+xInsomnian@users.noreply.github.com>
2022-11-18 21:55:34 -0600

    updating main file with docstrings

--- account.py
+++ account.py
@@ -1,9 +1,20 @@
 class Account:
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
+        """
+        Juts setting the account up with the name and balance
+        :param name:setting the name of the account
+        """
         self.__account_name = name
         self.__account_balance = 0
 
     def deposit(self, amount: float) -> bool:
+
+        """
+        the method for deposting in the account
+        :param amount: the amount set one wants to deposit
+        :return: True if it works and false if the amount was 0 or negative
+        """
+
         if amount > 0:
             self.__account_balance += amount
             return True
@@ -11,6 +22,12 @@ class Account:
             return False
 
     def withdraw(self, amount: float) -> bool:
+        """
+        This is the method for withdrawls
+        :param amount: the amount soemone wants to take out of the account
+        :return: True if it works and false if it doesnt because of a negative number or inssuficient funds
+        """
+
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
@@ -22,4 +39,3 @@ class Account:
 
     def get_name(self) -> str:
         return self.__account_name
-    
