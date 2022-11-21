1c7212272b7a4f7f90996fe55ef0ae48be74cf53
xInsomnian <98590267+xInsomnian@users.noreply.github.com>
2022-11-20 20:55:04 -0600

    updating test file with withdraw and deposit

--- test_account.py
+++ test_account.py
@@ -1,6 +1,7 @@
 import pytest
 from account import *
 
+
 class Test:
     def setup_method(self):
         self.a1 = Account('Joe')
@@ -13,7 +14,43 @@ class Test:
     def test_init(self):
         assert self.a1.get_name() == 'Joe'
         assert self.a2.get_name() == 'Jane'
+        assert self.a1.get_balance == pytest.approx(0, abs=0.001)
+        assert self.a2.get_balance == pytest.approx(0, abs=0.001)
 
     def test_deposit(self):
+        assert self.a1.deposit(30) is True
+        assert self.a1.get_balance == pytest.approx(30, abs=0.001)
+
+        assert self.a1.deposit(-30) is False
+        assert self.a1.get_balance == pytest.approx(30, abs=0.001)
+
+        assert self.a1.deposit(30.5) is True
+        assert self.a1.get_balance == pytest.approx(60.5, abs=0.001)
+
+        assert self.a2.deposit(25.5) is True
+        assert self.a2.get_balance == pytest.approx(25.5, abs=0.001)
+
+        assert self.a2.deposit(-30.5) is False
+        assert self.a2.get_balance == pytest.approx(25.5, abs=0.001)
+
+        assert self.a2.deposit(0) is False
+        assert self.a2.get_balance == pytest.approx(25.5, abs=0.001)
 
     def test_withdraw(self):
+        assert self.a1.deposit(30) is True
+        assert self.a1.get_balance == pytest.approx(30, abs=0.001)
+
+        assert self.a1.withdraw(20) is True
+        assert self.a1.get_balance == pytest.approx(10, abs=0.001)
+
+        assert self.a1.withdraw(5.5) is True
+        assert self.a1.get_balance == pytest.approx(4.5, abs=0.001)
+
+        assert self.a1.withdraw(0) is False
+        assert self.a1.get_balance == pytest.approx(4.5, abs=0.001)
+
+        assert self.a1.withdraw(-15.5) is False
+        assert self.a1.get_balance == pytest.approx(4.5, abs=0.001)
+
+        assert self.a1.withdraw(10) is False
+        assert self.a1.get_balance == pytest.approx(4.5, abs=0.001)
